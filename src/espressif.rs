67e53aa3e1731dbbe41a35ee5227373c9c215a40
Matthew J Perez <matt@mperez.io>
2025-03-21 15:30:24 -0400

    initializes i2c bus and bme280 driver

--- src/espressif.rs
+++ src/espressif.rs
@@ -3,6 +3,7 @@ use micro_rdk::common::config::ConfigType;
 use micro_rdk::common::i2c::{I2CHandle, I2cHandleType};
 use micro_rdk::common::registry::{ComponentRegistry, Dependency, RegistryError};
 use micro_rdk::common::status::{Status, StatusError};
+use micro_rdk::esp32::esp_idf_svc::hal::sys::esp;
 use micro_rdk::DoCommand;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
@@ -24,7 +25,8 @@ static mut BME280: bme280_handle_t = std::ptr::null_mut();
 //    I2C_MODE_MASTER,      /*!< I2C master mode */
 //    I2C_MODE_MAX,
 //} i2c_mode_t;
-const I2C_MODE_MASTER: u32 = 0x0;
+const I2C_MODE_MASTER: u32 = 0x1;
+const I2C_BUS_NO: i32 = 0;
 
 pub fn register_models(registry: &mut ComponentRegistry) -> Result<(), RegistryError> {
     registry.register_sensor("bme280", &Bme280::from_config)
@@ -54,16 +56,18 @@ impl Readings for Bme280 {
 
 impl SensorT<f64> for Bme280 {
     fn get_readings(&self) -> Result<TypedReadingsResult<f64>, SensorError> {
-        //Step3: Read temperature, humidity and pressure
-        let mut temperature: std::ffi::c_float = 0.0;
-        let mut pressure: std::ffi::c_float = 0.0;
-        let t_ptr = &mut temperature as *mut f32;
-        let p_ptr = &mut pressure as *mut f32;
+        let mut temperature: f32 = 0.0;
+        let mut pressure: f32 = 0.0;
         unsafe {
-            bme280_read_temperature(BME280, t_ptr);
+            log::info!("bme280 - reading temp...");
+            let r = bme280_read_temperature(BME280, &mut temperature as &mut f32);
+            log::warn!("res of reading: {}", r);
             //bme280_read_humidity(bme280, &humidity);
-            bme280_read_pressure(BME280, p_ptr);
+            log::info!("bme280 - reading pressure...");
+            let r = bme280_read_pressure(BME280, &mut pressure as &mut f32);
+            log::warn!("res of reading: {}", r);
         }
+        log::info!("temp: {} - pressure: {}", temperature, pressure);
         let mut x = HashMap::new();
         x.insert("temp_celsius".to_string(), temperature.into());
         x.insert("pressure".to_string(), pressure.into());
@@ -73,17 +77,30 @@ impl SensorT<f64> for Bme280 {
 
 impl Bme280 {
     pub fn from_config(cfg: ConfigType, deps: Vec<Dependency>) -> Result<SensorType, SensorError> {
-        let board = micro_rdk::common::registry::get_board_from_dependencies(deps);
-        if board.is_none() {
-            return Err(SensorError::ConfigError("BME280 missing board attribute"));
-        }
-        let board = board.unwrap();
-        let i2c_handle: I2cHandleType;
-        if let Ok(i2c_name) = cfg.get_attribute::<String>("i2c_bus") {
-            i2c_handle = board.get_i2c_by_name(i2c_name)?;
-        } else {
-            return Err(SensorError::ConfigError("BME280 missing i2c_bus attribute"));
-        }
+        // DO NOT use the board i2c interface to initialize the i2c bus, the idf-component will handle it
+        /*
+                //Step1: Init I2C bus
+                i2c_config_t conf = {
+                    .mode = I2C_MODE_MASTER,
+                    .sda_io_num = I2C_MASTER_SDA_IO,
+                    .sda_pullup_en = GPIO_PULLUP_ENABLE,
+                    .scl_io_num = I2C_MASTER_SCL_IO,
+                    .scl_pullup_en = GPIO_PULLUP_ENABLE,
+                    .master.clk_speed = I2C_MASTER_FREQ_HZ,
+                };
+                i2c_bus = i2c_bus_create(I2C_MASTER_NUM, &conf);
+
+                //Step2: Init bme280
+                bme280 = bme280_create(i2c_bus, BME280_I2C_ADDRESS_DEFAULT);
+                bme280_default_init(bme280);
+
+                //Step3: Read temperature, humidity and pressure
+                float temperature = 0.0, humidity = 0.0, pressure = 0.0;
+                bme280_read_temperature(bme280, &temperature);
+                bme280_read_humidity(bme280, &humidity);
+                bme280_read_pressure(bme280, &pressure);
+                }
+        */
 
         let config = i2c_config_t {
             mode: I2C_MODE_MASTER,
@@ -97,42 +114,15 @@ impl Bme280 {
             clk_flags: 0,
         };
 
-        let bus_no = match i2c_handle.name().as_str() {
-            "0" => 0,
-            "1" => 1,
-            _ => 0,
-        };
-
         unsafe {
-            I2C_BUS = i2c_bus_create(bus_no, &config);
-            BME280 = bme280_create(I2C_BUS, BME280_I2C_ADDRESS_DEFAULT.try_into().unwrap());
-            bme280_default_init(BME280);
+            log::info!("initializing I2C_BUS: {:?}", I2C_BUS);
+            I2C_BUS = i2c_bus_create(I2C_BUS_NO, &config);
+            log::info!("initialized I2C_BUS to : {:?}", I2C_BUS);
+            log::info!("initializing BME280: {:?}", BME280);
+            BME280 = bme280_create(I2C_BUS, BME280_I2C_ADDRESS_DEFAULT);
+            log::info!("initialized BME280 to : {:?}", BME280);
+            esp!(bme280_default_init(BME280))?;
         }
         Ok(Arc::new(Mutex::new(Self {})))
-        // use i2c_handle.name() to activate bus using esp-idf component
-
-        /*
-
-        //Step1: Init I2C bus
-        i2c_config_t conf = {
-            .mode = I2C_MODE_MASTER,
-            .sda_io_num = I2C_MASTER_SDA_IO,
-            .sda_pullup_en = GPIO_PULLUP_ENABLE,
-            .scl_io_num = I2C_MASTER_SCL_IO,
-            .scl_pullup_en = GPIO_PULLUP_ENABLE,
-            .master.clk_speed = I2C_MASTER_FREQ_HZ,
-        };
-        i2c_bus = i2c_bus_create(I2C_MASTER_NUM, &conf);
-
-        //Step2: Init bme280
-        bme280 = bme280_create(i2c_bus, BME280_I2C_ADDRESS_DEFAULT);
-        bme280_default_init(bme280);
-
-        //Step3: Read temperature, humidity and pressure
-        float temperature = 0.0, humidity = 0.0, pressure = 0.0;
-        bme280_read_temperature(bme280, &temperature);
-        bme280_read_humidity(bme280, &humidity);
-        bme280_read_pressure(bme280, &pressure);
-        }*/
     }
 }
